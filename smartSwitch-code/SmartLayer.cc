72e3261b54c531f488d575afe0813c2744cf964d
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-05-19 18:43:52 -0400

    added filter of dest ip address in smart switch!

--- smartSwitch-code/SmartLayer.cc
+++ smartSwitch-code/SmartLayer.cc
@@ -1,6 +1,6 @@
 #include "SmartLayer.h"
 
-namespace inet {
+namespace inet{
 
 Define_Module(SmartLayer);
 
@@ -10,19 +10,35 @@ void SmartLayer::initialize()
     this->num_of_tensors=0;
     this->down=0;
     this->first_tensor = NULL;
+    this->filter.setPattern("tcpseg*");
 }
 
-void SmartLayer::handleMessage(cPacket *msg)
+void SmartLayer::handleMessage(cMessage* msg)
 {
     // TODO - Generated method body
+    Packet* packet=check_and_cast<Packet*>(msg);
+
     if (msg->arrivedOn("fromUpperL")) { // if the message is already processed then just forward it to send
             down++;
-            PacketPrinter printer; // turns packets into human readable strings
-            printer.printPacket(std::cout, msg); // print to standard output
+
+            // checking the destination IP of received packet!
+            if(filter.matches(packet)){
+                const auto& my_ipv4header = packet->peekAtFront<Ipv4Header>();
+                auto destIP = my_ipv4header->getDestAddress();
+                if(to_check == destIP)
+                    EV_INFO << "FOUND FROM 10.0.0.1 TO 10.0.0.2!\n";
+            }
+
+            // Print received packet
+            PacketPrinter printer;
+            EV << printer.printPacketToString(packet) << endl;
+            //
             send(msg, "toLowerL");
     } else {
         error("arrived on unknown gate");
     }
+
+    // print some information above layer in simulation
     char buf[128];
     sprintf(buf, "tensors caught: %d, messages went through: %d", num_of_tensors, down);
     getDisplayString().setTagArg("t", 0, buf);
--- smartSwitch-code/SmartLayer.h
+++ smartSwitch-code/SmartLayer.h
@@ -2,7 +2,13 @@
 #define __INET_SMARTLAYER_H_
 
 #include <omnetpp.h>
-//#include "inet/common/MessageDispatcher.h"
+#include "inet/common/packet/printer/PacketPrinter.h"
+#include "inet/common/packet/Packet.h"
+#include "inet/common/packet/PacketFilter.h"
+#include "inet/networklayer/ipv4/Ipv4.h"
+#include <string.h>
+#include <iostream>
+#include <regex>
 
 using namespace omnetpp;
 
@@ -19,7 +25,10 @@ class SmartLayer : public cSimpleModule
   public:
       int num_of_tensors;
       int down;
-      cPacket* first_tensor;
+      Packet* first_tensor;
+      PacketFilter filter;
+      Ipv4Address to_check = Ipv4Address("10.0.0.2");
+
 };
 
 } //namespace
